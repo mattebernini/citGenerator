8aa4e80578610fb5df412d65ab7bdc77cd875efa
matte <matteebernini@gmail.com>
2021-04-16 08:36:02 +0200

    correzione lettera

--- citGenerator.sh
+++ citGenerator.sh
@@ -1,4 +1,4 @@
 cartella_attuale="$PWD"
-cd "/home/matte/Scrivania/Development/PROGETTI/nuoviProgetti/citGenerato"
+cd "/home/matte/Scrivania/Development/PROGETTI/nuoviProgetti/citGenerator"
 python3 citGenerator.py 
 cd "${cartella_attuale}"
